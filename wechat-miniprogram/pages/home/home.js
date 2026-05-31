b90aa52bc798a300101bad0a64bfe6fb2f8aa654
Luyi Wang <luyiwang1@users.noreply.github.com>
2026-05-31 01:24:38 -0400

    Add signup navigation from courts page

--- wechat-miniprogram/pages/home/home.js
+++ wechat-miniprogram/pages/home/home.js
@@ -2,7 +2,7 @@ const db = require('../../utils/db');
 
 Page({
   data: {
-    state: { title: '金鸽巡回赛', capacity: null, joined: [], waitlist: [] },
+    state: { title: 'Dove Cup', capacity: null, joined: [], waitlist: [] },
     idSeq: 1,
     nameInput: '',
     capacityInput: '',
